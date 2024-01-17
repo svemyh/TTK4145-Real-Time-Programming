e54b82a045d9bf6bc466c47307cc9982d43a55c0
Sorstronen <snorrehs@stud.ntnu.no>
2024-01-17 19:05:27 +0100

    Complete program from local Linux computer

--- Exercise 2/UDP TCP/TCP/Client.go
+++ Exercise 2/UDP TCP/TCP/Client.go
@@ -1,25 +1,26 @@
 package main
 
 import (
-    "fmt"
-    "net"
+	"fmt"
+	"net"
 )
 
 func main() {
-    // Connect to the server
-    conn, err := net.Dial("tcp", "10.24.37.52:20000")
-    if err != nil {
-        fmt.Println(err)
-        return
-    }
+	// Connect to the server
 
-    // Send some data to the server
-    _, err = conn.Write([]byte("Hello, server!"))
-    if err != nil {
-        fmt.Println(err)
-        return
-    }
+	conn, err := net.Dial("tcp", "10.100.23.23:20013")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
-    // Close the connection
-    conn.Close()
+	// Send some data to the server
+	_, err = conn.Write([]byte("Hello, server!"))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// Close the connection
+	conn.Close()
 }
--- Exercise 2/UDP TCP/TCP/Server.go
+++ Exercise 2/UDP TCP/TCP/Server.go
@@ -1,45 +1,45 @@
 package main
 
 import (
-    "fmt"
-    "net"
-  	"time"
+	"fmt"
+	"net"
+	"time"
 )
 
 func main() {
-    // Listen for incoming connections on port 8080
-    ln, err := net.Listen("tcp", ":20000")
-    if err != nil {
-        fmt.Println(err)
-        return
-    }
+	// Listen for incoming connections on port 8080
+	ln, err := net.Listen("tcp", ":20013")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
-    // Accept incoming connections and handle them
-    for {
-        conn, err := ln.Accept()
-        if err != nil {
-            fmt.Println(err)
-            continue
-        }
+	// Accept incoming connections and handle them
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
 
-        // Handle the connection in a new goroutine
-        go handleConnection(conn)
-    }
+		// Handle the connection in a new goroutine
+		go handleConnection(conn)
+	}
 }
 
 func handleConnection(conn net.Conn) {
-    // Close the connection when we're done
-    defer conn.Close()
+	// Close the connection when we're done
+	defer conn.Close()
 
-    // Read incoming data
-    buf := make([]byte, 1024)
-    _, err := conn.Read(buf)
-    if err != nil {
-        fmt.Println(err)
-        return
-    }
+	// Read incoming data
+	buf := make([]byte, 1024)
+	_, err := conn.Read(buf)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
-    // Print the incoming data
-    fmt.Printf("Received: %s", buf)
-  	time.Sleep(1 * time.Second)
+	// Print the incoming data
+	fmt.Printf("Received: %s", buf)
+	time.Sleep(1 * time.Second)
 }
