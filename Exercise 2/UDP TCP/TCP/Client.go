8168b8a268ec63993fa356d8a4360b1ea114bd8d
Anton Tran <antontr@stud.ntnu.no>
2024-01-24 07:03:49 +0100

    TCP update nr. idk

--- Exercise 2/UDP TCP/TCP/Client.go
+++ Exercise 2/UDP TCP/TCP/Client.go
@@ -5,22 +5,22 @@ import (
 	"net"
 )
 
+// Socket -> Connect -> Send <-> Recv (from server) -> Send -> Close
+
 func main() {
-	// Connect to the server
+	conn, err := net.Dial("tcp", "192.168.1.6:20000")
+	if err != nil {
+		fmt.Println("The connection failed. Error: ", err)
+		return
+	} else {
+		fmt.Printf("The connection was established to: %s \n", conn.RemoteAddr()) //wtf
+	}
 
-	conn, err := net.Dial("tcp", "10.100.23.23:20013")
+	_, err = conn.Write([]byte("Gruppe 13!"))
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	// Send some data to the server
-	_, err = conn.Write([]byte("Hello, server!"))
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	// Close the connection
-	conn.Close()
+	defer conn.Close()
 }
--- Exercise 2/UDP TCP/TCP/Server.go
+++ Exercise 2/UDP TCP/TCP/Server.go
@@ -6,40 +6,79 @@ import (
 	"time"
 )
 
+// Socket -> Bind -> Listen -> Accept -> Recv (from client) <-> Send -> Recv -> Close
+
 func main() {
-	// Listen for incoming connections on port 8080
-	ln, err := net.Listen("tcp", ":20013")
+
+	port := 20000
+	//Listen
+	//conn, err := net.Listen("tcp", fmt.Sprintf("%s:%d", ip, port))
+	ls, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("The connection failed. Error:", err)
 		return
 	}
+	defer ls.Close()
 
-	// Accept incoming connections and handle them
+	fmt.Println("Connected to port:", port)
 	for {
-		conn, err := ln.Accept()
+		conn, err := ls.Accept()
 		if err != nil {
-			fmt.Println(err)
+			fmt.Println("Error: ", err)
 			continue
 		}
 
-		// Handle the connection in a new goroutine
 		go handleConnection(conn)
+		time.Sleep(1 * time.Second)
 	}
 }
 
 func handleConnection(conn net.Conn) {
-	// Close the connection when we're done
 	defer conn.Close()
 
-	// Read incoming data
-	buf := make([]byte, 1024)
-	_, err := conn.Read(buf)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+	//Accept
+	fmt.Printf("Accepted connection from %s\n", conn.LocalAddr())
 
-	// Print the incoming data
-	fmt.Printf("Received: %s", buf)
-	time.Sleep(1 * time.Second)
+	//Send
+	msg := fmt.Sprintf("Connect to: %s\n", conn.LocalAddr())
+	conn.Write([]byte(msg))
 }
+
+// func main() {
+// 	// Listen for incoming connections on port 8080
+// 	ln, err := net.Listen("tcp", ":20013")
+// 	if err != nil {
+// 		fmt.Println(err)
+// 		return
+// 	}
+
+// 	// Accept incoming connections and handle them
+// 	for {
+// 		conn, err := ln.Accept()
+// 		if err != nil {
+// 			fmt.Println(err)
+// 			continue
+// 		}
+
+// 		// Handle the connection in a new goroutine
+// 		go handleConnection(conn)
+// 	}
+// }
+
+// func handleConnection(conn net.Conn) {
+// 	// Close the connection when we're done
+// 	defer conn.Close()
+
+// 	// Read incoming data
+// 	buf := make([]byte, 1024)
+// 	_, err := conn.Read(buf)
+// 	if err != nil {
+// 		fmt.Println(err)
+// 		return
+// 	}
+
+// 	// Print the incoming data
+// 	fmt.Printf("Received: %s", buf)
+// 	// To not get spammed
+// 	time.Sleep(1 * time.Second)
+// }
